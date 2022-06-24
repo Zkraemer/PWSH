060b261db74a23053ddec76b2ef949bd65ca7dd7
Zkraemer <52175468+Zkraemer@users.noreply.github.com>
2022-06-24 12:27:25 -0400

    Update CredentialBuild.ps1
    Included input validation "confirm passwords match" With error reporting if "passwords do not match"

--- CredentialBuild.ps1
+++ CredentialBuild.ps1
@@ -6,17 +6,49 @@ $domainNETBIOS = $domainFQDN.split('.')[0]
 
 #Define Local User
 $localUsername = Read-Host "Enter Local Username"
-$localPassword = Read-Host -AsSecureString -Prompt "Enter Local User Password"
+do {
+$localPassword = Read-Host -Prompt "Enter Local User Password" 
+$localPasswordConfirm = Read-Host -Prompt "Confirm Local User Password"
+If($localPassword -notlike $localPasswordConfirm){
+Write-Host "Passwords do not Match! Please try again" -ForegroundColor Red
+}else{
+$localPasswordSecure = $localPassword | ConvertTo-SecureString -AsPlainText -Force
+$localPassword = ""
+$localPasswordConfirm = ""
+}
+}while($localPassword -notlike $localPasswordConfirm)
 
 #Define Domain User
 $domainUsername = ("{0}/{1}" -f $domainNETBIOS, (Read-Host "Enter Domain Username"))
-$domainPassword = Read-Host -AsSecureString -Prompt "Enter Domain User Password"
+do {
+$domainPassword = Read-Host -Prompt "Enter Domain User Password" 
+$domainPasswordConfirm = Read-Host -Prompt "Confirm Domain User Password"
+If($domainPassword -notlike $domainPasswordConfirm){
+Write-Host "Passwords do not Match! Please try again" -ForegroundColor Red
+}else{
+$domainPasswordSecure = $domainPassword | ConvertTo-SecureString -AsPlainText -Force
+$domainPassword = ""
+$domainPasswordConfirm = ""
+}
+}while($domainPassword -notlike $domainPasswordConfirm)
+
 
 #Define Console User
 $conUsername = Read-Host "Enter Console Username"
 $conPassword = Read-Host -AsSecureString -Prompt "Enter Console Password"
+do {
+$conPassword = Read-Host -Prompt "Enter Console User Password" 
+$conPasswordConfirm = Read-Host -Prompt "Confirm Console User Password"
+If($conPassword -notlike $conPasswordConfirm){
+Write-Host "Passwords do not Match! Please try again" -ForegroundColor Red
+}else{
+$conPasswordSecure = $conPassword | ConvertTo-SecureString -AsPlainText -Force
+$conPassword = ""
+$conPasswordConfirm = ""
+}
+}while($conPassword -notlike $conPasswordConfirm)
 
 #Build Secure PSCredential
-$localCredential = [System.Management.Automation.PSCredential]::new($localUsername,$localPassword)
-$domainCredential = [System.Management.Automation.PSCredential]::new($domainUsername,$domainPassword)
-$consoleCredential = [System.Management.Automation.PSCredential]::new($conUsername,$conPassword)
+$localCredential = [System.Management.Automation.PSCredential]::new($localUsername,$localPasswordSecure)
+$domainCredential = [System.Management.Automation.PSCredential]::new($domainUsername,$domainPasswordSecure)
+$consoleCredential = [System.Management.Automation.PSCredential]::new($conUsername,$conPasswordSecure)
