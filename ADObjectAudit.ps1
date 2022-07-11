c4c159b3763cc581a53437302e33666fc9a2c8f7
Zkraemer <52175468+Zkraemer@users.noreply.github.com>
2022-07-11 10:14:31 -0400

    Update ADObjectAudit.ps1
    Import names from CSV

--- ADObjectAudit.ps1
+++ ADObjectAudit.ps1
@@ -1,11 +1,28 @@
-##Convert Time from ticks to date/time format
-#@{Name='LastLogonTimestamp';Expression={[DateTime]::fromfiletime($_.lastlogontimestamp)}}
+$runTime = get-date -Format "yyyyMMddHHmm"
+$servers = [System.Collections.ArrayList]::new()
 
-#List of objects to collect
-$servers = "Server-Name"
+##Add names from a CS
+$getCSV = Import-Csv -Path #Enter Path Here
+$getCSV | foreach {
+$name = $_ | select -ExpandProperty #Property representing ADObject Name
+$servers.Add($name)
+}
 
-#Collects the Last Logon Timestamp, Password Last Set, Operating System, and Operating System Version properties and saves them to the desktop.
+##Add names individually
+<#
+do{
+$servername = Read-Host "Please enter Host name"
+$servers.add(($servername))
+}while($servername -notlike $null)
+$servers.remove("")
+#>
+
 $servers | foreach{
-    Get-ADObject -SearchBase "ou=_clients,dc=arthrex,dc=local" -Filter * -Properties lastlogontimestamp, pwdlastset,operatingsystem,operatingsystemversion|
-    where -Property name -match $_ | select Name,@{Name='LastLogonTimestamp';Expression={[DateTime]::fromfiletime($_.lastlogontimestamp)}}, @{Name='pwdlastset';Expression={[DateTime]::fromfiletime($_.pwdlastset)}},operatingsystem,operatingsystemversion,objectclass,distinguishedname | fl
-} | Out-File -FilePath C:\Users\admin_zk11354\Desktop\AuditADObjects.txt
+    Get-ADObject -SearchBase <#Enter a Searchbase Here#> -Filter * -Properties lastlogontimestamp, pwdlastset,operatingsystem,operatingsystemversion|
+    where -Property name -match $_ | 
+        select Name,@{Name='LastLogonTimestamp';Expression={[DateTime]::fromfiletime($_.lastlogontimestamp)}}, @{Name='pwdlastset';Expression={[DateTime]::fromfiletime($_.pwdlastset)}},operatingsystem,operatingsystemversion | 
+        fl | Out-File -FilePath ("C:\Users\admin_zk11354\Desktop\AuditADObjects{0}.txt" -f $runTime) -Append
+    Write-host ("{0} added to file" -f $_) -ForegroundColor Cyan
+}
+
+#@{Name='LastLogonTimestamp';Expression={[DateTime]::fromfiletime($_.lastlogontimestamp)}}
