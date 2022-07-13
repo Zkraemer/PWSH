3c972be04d5398aae6b1dfd8bd5a574e58289f5f
Zkraemer <52175468+Zkraemer@users.noreply.github.com>
2022-07-13 15:21:47 -0400

    Update ADUserLastNameFix.ps1
    fixed error in ADUser object handling

--- ADUserLastNameFix.ps1
+++ ADUserLastNameFix.ps1
@@ -8,7 +8,7 @@ $userOU = @(
 
 #get a list of Admin Users with no surname
 $UsersNoSurname = $userOU | foreach{get-aduser -SearchBase $_ -Filter * -Properties givenName,surname | where -Property surname -eq $null}
-$AdminUsersNoSurname = $UsersNoSurname | foreach{get-aduser -SearchBase $_ -Filter * -Properties givenName,surname | where -Property name -like "Admin*" | select -Property name,givenName,surname} 
+$AdminUsersNoSurname = $UsersNoSurname | foreach{get-aduser -SearchBase $_ -Filter * -Properties givenName,surname | where -Property name -like "Admin*"}
 Write-Host ("Total of {0} users collected." -f $AdminUsersNoSurname.count) -ForegroundColor Yellow
 
 $count = 0
@@ -16,10 +16,10 @@ $AdminUsersNoSurname | foreach {
     $userSurname = $_.givenname.split(" ")[-1]
     $userGivenName = $_.givenname -replace $userSurname
         
-    #$_ | Set-ADUser -Surname "$userSurname"
+    $_ | Set-ADUser -Surname "$userSurname" 
     Write-host ("Set {0} surname to {1}" -f $_.name, $userSurname) -ForegroundColor Cyan 
     
-    #$_ | Set-ADUser -GivenName $userGivenName
+    $_ | Set-ADUser -GivenName $userGivenName 
     Write-host ("Changed {0} givenName to {1}" -f $_.name, $userGivenName) -ForegroundColor DarkYellow
     Write-Host " "
 
