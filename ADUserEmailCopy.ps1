395dd314ac6e570b5b5f2c751882af07c60a6fc1
Zkraemer <52175468+Zkraemer@users.noreply.github.com>
2022-07-13 15:54:39 -0400

    Update ADUserEmailCopy.ps1
    Added the ability to dynamically add/remove OU's to a variable

--- ADUserEmailCopy.ps1
+++ ADUserEmailCopy.ps1
@@ -2,26 +2,33 @@
 #Get ADUsers who match Admin_* Names
 #Copy Email so that both are the same
 
+$adminUserOU = @("<#Enter Admin User Org Unit Here#>")
+$userOU = @("<#Enter User Org Units Here#>")
+
 #get a list of Admin Users
-$adminUsers = get-aduser -SearchBase <#SearchBase#> -Filter * -Properties mail | where -Property distinguishedname -like "CN=Admin_*" | select -Property distinguishedname,name,mail
+$adminUsers = get-aduser -SearchBase "$adminUserOU" -Filter * -Properties mail | where -Property sAMaccountName -like "Admin_*"
 
 #get a list of regular Users
-$Users = get-aduser -SearchBase <#SearchBase#> -Filter * -Properties mail | select -Property distinguishedname,name,mail
+$Users = get-aduser -SearchBase "$userOU" -Filter * -Properties mail
 
 #Get a list of Admin Users missing email addresses
-$missingMailAttribute = $adminUsers | where -Property mail -EQ $null
+$usersMissingMailAttribute = $adminUsers | where -Property mail -EQ $null
 
 #Pull the names and remove Admin_ to compare
-$missingMailAttributeName = $missingMailAttribute.name | foreach {$_.split(",")[0].split("_")[1]}
+$usersMissingMailAttributeName = $usersMissingMailAttribute.sAMAccountName | foreach {$_.split("_")[1]}
 
 #for every user Missing an Email address, Find the Email address of their regular user account and assign it to their Admin acount.
-foreach ($name in $missingMailAttributeName){
-    if ($name -in $users.name){
-    $mailAddress = get-aduser -SearchBase <#SearchBase#> -Filter * -Properties mail | where -Property name -eq $name | select -ExpandProperty mail
-    $user = get-aduser -SearchBase <#SearchBase#> -Filter * -Properties mail | where -Property name -like "Admin_$name"
-    $user | Set-ADUser -Add @{mail="$mailaddress"} -Verbose
+$count=0
+foreach ($name in $usersMissingMailAttributeName){
+    if ($name -in $users.sAMAccountName){
+    $mailAddress = get-aduser -SearchBase $userOU -Filter * -Properties mail | where -Property sAMAccountName -eq $name | select -ExpandProperty mail
+    $adminUser = get-aduser -SearchBase $adminUserOU -Filter * -Properties mail | where -Property sAMAccountname -like "Admin_$name"
+    $adminUser | Set-ADUser -Add @{mail="$mailaddress"} -ErrorAction Continue
+    Write-Host ("Admin_{0} now has an Email of: {1}" -f $name,$mailAddress) -ForegroundColor Cyan
+    $count++
     }
 }
+Write-host ("Total number of users modified: {0}" -f $count) -ForegroundColor Yellow
 
 <#Notes
 
