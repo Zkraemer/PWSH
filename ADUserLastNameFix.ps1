feb27916dc5b66c56918037a75e075db2d1c73da
Zkraemer <52175468+Zkraemer@users.noreply.github.com>
2022-07-14 09:56:25 -0400

    Update ADUserLastNameFix.ps1
    Added Count validation for less than 2 collected $AdminUsersNoSurname.
    
    Forced $userSurname and $userGivenName attributes to use Title Case

--- ADUserLastNameFix.ps1
+++ ADUserLastNameFix.ps1
@@ -1,7 +1,9 @@
 #Get ADUsers that do not have last names
 #Copy the last, name in the "First Name" Attribute to the "Last Name" Attribute
 #Remove that last, name from the "First Name" Attribute
+$textinfo = (Get-Culture).TextInfo
 
+#Enter User OU's
 $userOU = @(
 <#Enter OU's you wish to search here#>
 )
@@ -9,17 +11,23 @@ $userOU = @(
 #get a list of Admin Users with no surname
 $UsersNoSurname = $userOU | foreach{get-aduser -SearchBase $_ -Filter * -Properties givenName,surname | where -Property surname -eq $null}
 $AdminUsersNoSurname = $UsersNoSurname | foreach{get-aduser -SearchBase $_ -Filter * -Properties givenName,surname | where -Property name -like "Admin*"}
-Write-Host ("Total of {0} users collected." -f $AdminUsersNoSurname.count) -ForegroundColor Yellow
+if ($AdminUsersNoSurname.count -ge 2){
+    Write-Host ("Total of {0} users collected." -f $AdminUsersNoSurname.count) -ForegroundColor Yellow
+}elseif ($AdminUsersNoSurname -notlike $null){
+    Write-Host ("Total of {0} users collected." -f "1") -ForegroundColor Yellow
+}else{
+    Write-Host ("Total of {0} users collected." -f "0") -ForegroundColor Yellow
+}
 
 $count = 0
 $AdminUsersNoSurname | foreach {
-    $userSurname = $_.givenname.split(" ")[-1]
-    $userGivenName = $_.givenname -replace $userSurname
+    $userSurname = $textinfo.ToTitleCase($_.givenname.split(" ")[-1])
+    $userGivenName = $textinfo.ToTitleCase($_.givenname) -replace $userSurname
         
-    $_ | Set-ADUser -Surname "$userSurname" 
+    $_ | Set-ADUser -Surname "$userSurname" -WhatIf
     Write-host ("Set {0} surname to {1}" -f $_.name, $userSurname) -ForegroundColor Cyan 
     
-    $_ | Set-ADUser -GivenName $userGivenName 
+    $_ | Set-ADUser -GivenName $userGivenName -WhatIf
     Write-host ("Changed {0} givenName to {1}" -f $_.name, $userGivenName) -ForegroundColor DarkYellow
     Write-Host " "
 
