bab7e15ab41a07171f5cd4d97827109165839898
Zkraemer <52175468+Zkraemer@users.noreply.github.com>
2022-07-15 11:55:23 -0400

    Create ADUserCleanup.ps1

---
+++ ADUserCleanup.ps1
@@ -0,0 +1,34 @@
+##Used to Standardize your users in Active Directory
+
+$userOUs = @(<#Enter OU of Users#>)
+$domain = <#yourDomain.com#>
+
+$users = $userOUs | foreach {Get-ADUser -filter * -SearchBase $_}
+write-host ("Total users in Selection: {0}" -f $allUsers.count)
+
+foreach ($user in $users) {
+    $givenName = $textinfo.ToTitleCase($user.givenName)
+    $surname = $textinfo.ToTitleCase($user.surname)
+    $name = "{0} {1}" -f $givenName, $surname
+    $mail = $textinfo.tolowercase($user.mail)
+
+    if($user.givenName -notmatch $givenName){
+        $user | Set-ADUser -GivenName $givenName -WhatIf -Verbose
+    }
+
+    if($user.surname -notmatch $surname){
+        $user | Set-ADUser -surname $surname -WhatIf -Verbose
+    }
+
+    if($user.name -notmatch $name){
+        $user | Set-ADUser -Add @{name = $name} 
+        $user | Set-ADUser -Add @{displayname = $name} 
+    }
+    
+    if($user.mail -eq $null){
+        Set-ADUser -Add @{mail = "{0}.{1}@{2}" -f $givenName,$surname,$domain} #adjust this field to match your domain mail format current first.last@domain.com
+        }elseif($user.mail -notmatch $mail){
+          $user | Set-ADUser -Add @{name = $name}
+        }
+    }
+}
