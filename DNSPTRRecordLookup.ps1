fc785ec567575667c2da60009d3d74e3ed9d91ab
Zkraemer <52175468+Zkraemer@users.noreply.github.com>
2022-07-18 12:49:15 -0400

    Create DNSPTRRecordLookup.ps1
    Script to look through reverse lookup zones on specific DNS servers and Identify specific PTR records.

---
+++ DNSPTRRecordLookup.ps1
@@ -0,0 +1,20 @@
+#List of DNS Servers to Scan
+$dnsServers = @(
+<#Enter list of servers you wish to search here#>
+"",
+""
+)
+
+#Scan each Server in the list to Identify a specific DNS PTR Record
+foreach ($server in $dnsservers){
+    $reverseLookupZones = (Get-DnsServerZone -ComputerName $server | where {$_.isreverselookupzone -eq "true"}).ZoneName
+    
+    $records = $reverseLookupZones | foreach {Get-DnsServerResourceRecord -computername "$server" -RRType Ptr -ZoneName "$_" |
+    where -Property name -EQ "<#Enter the Record Name Here#>" -ErrorAction SilentlyContinue
+    }
+    if ($records -ne $null){
+        $records | out-file -FilePath "~\desktop\DNSReverseLookupResults.txt" -Append
+    }else{
+       "No Records Found" | out-file -FilePath "~\desktop\DNSReverseLookupResults.txt" -Append
+    }
+} 
