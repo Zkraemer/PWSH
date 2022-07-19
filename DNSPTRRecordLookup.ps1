2a9581308b76d42098092ea0a9280478b04aff2d
Zkraemer <52175468+Zkraemer@users.noreply.github.com>
2022-07-19 10:10:50 -0400

    Update DNSPTRRecordLookup.ps1

--- DNSPTRRecordLookup.ps1
+++ DNSPTRRecordLookup.ps1
@@ -1,21 +1,32 @@
 #List of DNS Servers to Scan
 $dnsServers = @(
-<#Enter list of servers you wish to search here#>
+<#Enter Servers to Scan Here#>
 "",
 ""
 )
+$targetRecord = ""<#Enter Record IP you'd like to lookup#>
 
-#Scan each Server in the list to Identify a specific DNS PTR Record
+#Scan each Server in the list to Identify a specific DNS PTR Record with matching name
 foreach ($server in $dnsservers){
     $reverseLookupZones = (Get-DnsServerZone -ComputerName $server | where {$_.isreverselookupzone -eq "true"}).ZoneName
     
-    $records = $reverseLookupZones | foreach {Get-DnsServerResourceRecord -computername "$server" -RRType Ptr -ZoneName "$_" |
-    where -Property name -EQ "<#Enter the Record Name Here#>" -ErrorAction SilentlyContinue
+    $records = $reverseLookupZones | foreach  {
+        Get-DnsServerResourceRecord -computername "$server" -RRType Ptr -ZoneName "$_" |
+        where -Property hostname -Like ($targetRecord.split(".")[-1]) -ErrorAction SilentlyContinue
     }
-    if ($records -ne $null){
+    
+    if ($records -notlike $null){
         $records | out-file -FilePath "~\desktop\DNSReverseLookupResults.txt" -Append
-        ("Total Records Found on {0}: {1}" -f $server,$records.count)
+        ("Total Records Found on {0}: {1}" -f $server,$records.count) | out-file -FilePath "~\desktop\DNSReverseLookupResults.txt" -Append
     }else{
-       ("No Records Found on {0}" -f $server) | out-file -FilePath "~\desktop\DNSReverseLookupResults.txt" -Append
+        ("No Records Found on {0}" -f $server) | out-file -FilePath ("~\desktop\DNSReverseLookupResults_for_{0}.txt" -f $targetRecord) -Append
     }
 } 
+
+
+#Notes
+<#
+Use this script to find PTR records in the DNS Reverse Lookup Zones on specific Servers.
+This script will pull all files that match the 4 Quartet of the IP address.
+Use this to Identify if there are mismatches or errors across your Domains DNS.
+#>
