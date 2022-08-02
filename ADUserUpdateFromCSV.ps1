4f0bbf678737bcfab42cface491db95215214692
Zkraemer <52175468+Zkraemer@users.noreply.github.com>
2022-08-02 10:13:52 -0400

    Create ADUserUpdateFromCSV.ps1
    Used to update user attributes.

---
+++ ADUserUpdateFromCSV.ps1
@@ -0,0 +1,14 @@
+##CSV Headers Required: name,mail,telephoneNumber
+
+$csv = Import-Csv -Path "Replace with CSV Path"
+$adUsers = get-aduser -filter * -SearchBase "Replace with OU or Remove Parameter for whole Domain" -Properties name,mail,telephoneNumber
+
+#Uses the mail property of the user to identify user then assign appropriate telephoneNumber
+$usersCSV = $csv | foreach { $adUsers |
+where -property mail -match $_.mail | 
+Set-ADUser -Add @(telephoneNumber="$csv.telephoneNumber")}
+
+#Notes
+<#
+
+#>
